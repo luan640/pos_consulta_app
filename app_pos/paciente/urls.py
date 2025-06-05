e16f3ab6ea8c14ab94fadc783190bdab5a8035ce
luan <luan640@gmail.com>
2025-06-04 22:48:11 -0300

    crud de regras finalizado

--- app_pos/paciente/urls.py
+++ app_pos/paciente/urls.py
@@ -11,4 +11,9 @@ urlpatterns += [
     path('api/materiais/', views.listar_materiais, name='api_listar_materiais'),
     path('api/registrar-contato/', views.registrar_contato, name='api_resistrar_contato'),
     path('api/paciente/<int:pk>/', views.paciente_detalhe, name='paciente_detalhe'),
+    path('api/regras/', views.regras_list_create, name='regras_list_create'),
+    path('api/regras/<int:pk>/', views.regras_detail_update, name='regras_detail_update'),
+    path('api/regras/<int:pk>/mover-up/', views.regra_mover_up, name='regra_mover_up'),
+    path('api/regras/<int:pk>/mover-down/', views.regra_mover_down, name='regra_mover_down'),
+
 ]
