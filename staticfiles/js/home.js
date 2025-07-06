06713c3ca45a78f6b64fd1d57eb8ddb35452dc06
luan <luan640@gmail.com>
2025-07-06 17:03:36 -0300

    iniciar selecao de materiais

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   inicializarVoltarEditarGrupo();
   inicializarFormularioAtribuirGrupo();
   inicializarEdicaoPaciente();
+  inicializarSelecaoMateriais();
 
 });
 
